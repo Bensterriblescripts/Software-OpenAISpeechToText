ca88e1b0f14164c5b73be7a1085fdfd877c1bda2
Ben <104821788+Bensterriblescripts@users.noreply.github.com>
2024-03-17 05:40:30 +1300

    finished dict, started on output stream for translations

--- texttospeech/src/converter.rs
+++ texttospeech/src/converter.rs
@@ -1,34 +1,42 @@
-use std::fs;
+use std::{fs, mem, u32};
 use std::fs::File;
 use std::io::Read;
 use std::time::Duration;
 use std::env;
 
-use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+use cpal::traits::*;
 use reqwest::blocking::Client;
 use reqwest::header::{HeaderMap, HeaderValue};
 
-pub fn write_audio() -> Option<cpal::Stream> {
+pub fn write_input_audio() -> Option<cpal::Stream> {
 
-    // Device
+    // Device - Input
     let host = cpal::default_host();
     let device = host
         .default_input_device()
         .expect("Failed to get default input device");
-    let supported_config = device.default_input_config().expect("Failed to get default input format");
-    let config: cpal:: StreamConfig = supported_config.into();
+    let supported_config = device
+        .default_input_config()
+        .expect("Failed to get default input format");
+    let config = cpal::StreamConfig {
+        channels: supported_config.channels(),
+        sample_rate: supported_config.sample_rate(),
+        buffer_size: cpal::BufferSize::Default
+    };
+
 
     // Wav file
     let mut file = hound::WavWriter::create("output.wav", hound::WavSpec {
-        channels: config.channels as u16,
-        sample_rate: config.sample_rate.0,
+        channels: 1,
+        sample_rate: 48000,
         bits_per_sample: 16,
         sample_format: hound::SampleFormat::Int,
 
     }).expect("Failed to create the wav file.");
 
+    let timeout = Duration::from_secs(3);
+
     // Input Stream
-    let timeout = Some(Duration::from_secs(3));
     let stream = device
         .build_input_stream(
             &config,
@@ -38,12 +46,62 @@ pub fn write_audio() -> Option<cpal::Stream> {
                 }
             },
             move |err| eprintln!("Error in stream: {}", err),
-            timeout,
+            Some(timeout)
         )
         .expect("Failed to build input stream");
 
+        println!("Device: {:?}", device.name().unwrap());
+
+    // Record the audio
+    stream.play().expect("Failed to start stream");
+
+    return Some(stream);
+}
+pub fn write_output_audio() -> Option<cpal::Stream> {
+
+    let available_hosts = cpal::OutputDevices();
+    println!("Available hosts: {:?}", available_hosts);
+
+    // Device - Output
+    let host = cpal::default_host();
+    let device = host
+        .default_output_device()
+        .expect("Failed to get default output device");
+    let supported_config = device
+        .default_output_config()
+        .expect("Failed to get default output format");
+    let config = cpal::StreamConfig {
+        channels: supported_config.channels(),
+        sample_rate: supported_config.sample_rate(),
+        buffer_size: cpal::BufferSize::Default
+    };
+
+    // Wav file
+    let mut file = hound::WavWriter::create("output.wav", hound::WavSpec {
+        channels: supported_config.channels(),
+        sample_rate: supported_config.sample_rate().0,
+        bits_per_sample: 16,
+        sample_format: hound::SampleFormat::Int,
+
+    }).expect("Failed to create the wav file.");
+
+    let timeout = Duration::from_secs(3);
+
+    // Output Stream
+    let stream = device
+        .build_output_stream(
+            &config,
+            move |data: &mut [i16], _: &cpal::OutputCallbackInfo| {
+                for sample in data {
+                    file.write_sample(*sample).expect("Failed to write audio data to file");
+                }
+            },
+            move |err| eprintln!("Error in stream: {}", err),
+            Some(timeout)
+        )
+        .expect("Failed to build output stream");
+
     println!("Device: {:?}", device.name().unwrap());
-    println!("Supported Config: {:?}", config);
 
     // Record the audio
     stream.play().expect("Failed to start stream");
@@ -82,10 +140,10 @@ pub fn send_request() -> Result<String, Box<dyn std::error::Error>> {
     let response_text = response.text().unwrap();
 
     // Delete the wav file
-    match fs::remove_file("output.wav") {
-        Ok(_) => (),
-        Err(e) => eprintln!("Error deleting the wav file: {}", e)
-    }
+    // match fs::remove_file("output.wav") {
+    //     Ok(_) => (),
+    //     Err(e) => eprintln!("Error deleting the wav file: {}", e)
+    // }
 
     Ok(response_text)
 }
--- texttospeech/src/main.rs
+++ texttospeech/src/main.rs
@@ -10,10 +10,15 @@ use winapi::um::winuser::GetAsyncKeyState;
 
 fn main() {
 
+    // Main Config
+    let mode = "output"; // microphone input: "input", computer output: "output"
+    let output_mode = "console"; // auto-typer: "type" or print to console: "console"
+    let spelling = false; // spell checker
+
+    // Keybind Loop
     let mut enigo = Enigo::new();
     let mut stream: Option<cpal::Stream> = None;
     let mut key_active = false;
-
     loop {
 
         // Check for our keybinds
@@ -22,8 +27,14 @@ fn main() {
 
         // Keydown
         if keystate_one != 0 && keystate_two != 0 && key_active == false {
-            stream = converter::write_audio();
+            if mode == "input" {
+                stream = converter::write_input_audio();
+            }
+            else if mode == "output" {
+                stream = converter::write_output_audio();
+            }
             key_active = true;
+            println!("Active");
         }
 
         // Keyup
@@ -34,7 +45,8 @@ fn main() {
 
                 // Pause the stream
                 if let Some (stream_ref) = stream.as_ref() {
-                    stream_ref.pause().expect("Failed to pause the stream")
+                    stream_ref.pause().expect("Failed to pause the stream");
+                    println!("Inactive");
                 }
 
                 // Send the API request
@@ -43,20 +55,28 @@ fn main() {
 
                         // Parse for only the text value
                         let text_value: Value = serde_json::from_str(api_response.as_str()).expect("Failed to serialise");
-                        let mut text = text_value["text"].as_str().unwrap_or_default();
+                        let mut text: &str = text_value["text"].as_str().unwrap_or_default();
 
                         // Check the spelling
-                        match grammar::check_spelling(text) {
-                            Ok(spellchecked_text) => {
-                                text = spellchecked_text;
-                            },
-                            Err(err) => {
-                                println!("Error checking the spelling {}", err);
+                        if spelling {
+                            match grammar::check_spelling(text) {
+                                Ok(spellchecked_text) => {
+                                    text = spellchecked_text;
+                                },
+                                Err(err) => {
+                                    println!("Error checking the spelling {}", err);
+                                }
                             }
                         }
 
-                        // Write the &str to the screen
-                        enigo.key_sequence(text);
+                        if output_mode == "type" {
+                            // Write the &str to the screen
+                            enigo.key_sequence(text);
+                        }
+                        else if output_mode == "console" {
+                            // Write the &str to console
+                            println!("Response: {}", text);
+                        }
                     }
                     Err(err) => {
                         eprintln!("Error sending request: {}", err);
